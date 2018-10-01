8ade5de6e4157862bae51a7538b8b66ac6036b7f
Ivan <obat.box@gmail.com>
2018-10-01 18:39:46 +0500

    refactored pdotest page

--- public/index.php
+++ public/index.php
@@ -13,17 +13,23 @@ $app->get('/', function() {
     return 'Main page';
 });
 
-/*
-$app->get('/about', function() {
-    return 'About page';
+$app->get('/about', function($request, $response, $args) {
+    return $this->renderer->render($response, "/about.phtml", $args);
 });
- */
+
 
 $container = $app->getContainer();
 $container['renderer'] = new PhpRenderer("../templates");
 
-$app->get('/about', function($request, $response, $args) {
-    return $this->renderer->render($response, "/about.phtml", $args);
+$app->get('/pdotest', function() {
+    $conn = new PDO('mysql:host=localhost;dbname=hexlet', 'root', '1');
+    $result = $conn->query("SELECT * FROM ads");
+    foreach ($result as $row) {
+        echo $row['id'] . '<br>';
+        echo $row['title'] . '<br>';
+        echo $row['telephone'] . '<br>';
+        echo '<br>';
+    }
 });
 
 $app->get('/hello/{name}', function($request, $response, $args) {
